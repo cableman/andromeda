105f72dd7b3f6bea6c1fd8365c52779ebfeaa89c
Jesper Kristensen <cableman@linuxdev.dk>
2010-11-08 19:39:25 +0100

    Added css and basic style

---
+++ sites/all/themes/andromeda/template/nodes/node-static_page.tpl.php
@@ -0,0 +1,14 @@
+<div id="node-<?php print $node->nid; ?>" class="node<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?> clear-block">
+
+<?php print $picture ?>
+
+<?php if (!$page): ?>
+  <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
+<?php endif; ?>
+  
+  <div class="content">
+    <?php print $content ?>
+  </div>
+
+  <?php print $links; ?>
+</div>
