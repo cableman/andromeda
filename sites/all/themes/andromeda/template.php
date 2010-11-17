68a133dc502eb1a27f550ef1c0310ed562f5b9ef
Jesper Kristensen <cableman@linuxdev.dk>
2010-11-17 21:38:19 +0100

    Added some styling "submitted by"

---
+++ sites/all/themes/andromeda/template.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Implementation of hook_preproces_node
+ *
+ * @param array $vars
+ */
+function andromeda_preprocess_node(&$vars) {
+  $node = $vars['node'];
+
+  // Submitted for news
+  if ($node->type == 'news') {
+    $date = andromeda_date_format(date_make_date($vars['created']), 'd. F Y');
+    $vars['submitted'] = t('Written d. !date by !author', array('!date' => $date, '!author' => $node->name));
+  }
+}
+
+/**
+ * Format an date in respect to the language code given.
+ *
+ * @param date object $date
+ * @param string $format
+ * @param string $lang
+ * @return string
+ */
+function andromeda_date_format($date, $format, $lang = 'da') {
+  return date_format_date($date, 'custom', $format, $lang);
+}
