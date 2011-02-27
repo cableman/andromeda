77901f2f13c9aee84d263f5b71df0c0f8d6d838c
Jesper Kristensen <cableman@linuxdev.dk>
2011-02-27 18:17:17 +0100

    Updated js

--- sites/all/themes/andromeda/js/fix_ei.js
+++ sites/all/themes/andromeda/js/fix_ei.js
@@ -1,13 +1,28 @@
+/* This is not a gode solution, but it works for now */
 $(document).ready(function () {
-  /* This is not a gode solution, but it works for now */
-  var img = $('.field-field-content img');
-  var style = img.attr('style');  
-  if (style.indexOf('float: left;') > -1) {
-    img.addClass('left');
-  }
 
-  if (style.indexOf('float: right;') > -1) {
-    img.addClass('right');
+  // Select all images on the page
+  var imgs = $('#content .field-field-content img');
+  if (imgs.length == 0) { return; }
+
+  // Run through the images
+  for (var i = 0; imgs.length > i; i++) {
+    var img = $(imgs[i]);
+
+    // Get style attribute
+    var style = img.attr('style');
+
+    // Add class to left float
+    if (style.indexOf('float: left;') > -1) {
+      img.addClass('left');
+    }
+
+    // Add class to right float
+    if (style.indexOf('float: right;') > -1) {
+      img.addClass('right');
+    }
+
+    alert('test');
   }
 });
 
