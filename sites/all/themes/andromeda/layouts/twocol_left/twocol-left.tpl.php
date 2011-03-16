8a52a31d182479585698cc99f6ce03086951e0c6
Jesper Kristensen <cableman@linuxdev.dk>
2011-03-16 22:18:04 +0100

    Update fotoablum panels

--- sites/all/themes/andromeda/layouts/twocol_left/twocol-left.tpl.php
+++ sites/all/themes/andromeda/layouts/twocol_left/twocol-left.tpl.php
@@ -1,11 +1,11 @@
   <?php if ($content['left']) { ?>
-    <div class="grid-9 alpha panel-col-left <?php if (!$content['right']) { ?>suffix-3 omega<?php } ?>">
+    <div class="grid-8 alpha panel-col-left <?php if (!$content['right']) { ?>suffix-4 omega<?php } ?>">
       <?php print $content['left']; ?>
     </div>
   <?php } ?>
   
   <?php if ($content['right']) { ?>
-  <div class="<?php if (!$content['left']) { ?>prefix-9 alpha <?php } ?> grid-3 omega panel-col-right">
+  <div class="<?php if (!$content['left']) { ?>prefix-8 alpha <?php } ?> grid-4 omega panel-col-right">
     <?php print $content['right']; ?>
   </div>
   <?php } ?>
