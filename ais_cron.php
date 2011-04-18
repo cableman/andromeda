9a06a3d12abfa166d2609b17d2a7bd4e006b3df1
Jesper Kristensen <cableman@linuxdev.dk>
2011-04-18 22:34:58 +0200

    Fixed problem with to many saved AIS points

--- ais_cron.php
+++ ais_cron.php
@@ -1,5 +1,33 @@
 <?php
 
+/**
+ * Find distance between to geo points.
+ */
+function ais_distance($lat1, $lon1, $lat2, $lon2) {
+  $theta = $lon1 - $lon2;
+  $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+  $dist = acos($dist);
+  $dist = rad2deg($dist);
+  $miles = $dist * 60 * 1.1515;
+  $unit = strtoupper($unit);
+
+  return $miles * 0.8684;
+}
+
+/**
+ * Insert new AIS point into the database.
+ */
+function ais_insert($values) {
+  db_query("INSERT INTO {ais} VALUES ('', %f, %f, '%s', %d, %d, '%s', %f, %f, %d, %d)", $values);
+}
+
+/**
+ * Update AIS point in the database.
+ */
+function ais_update($id, $values) {
+  db_query('UPDATE {ais} SET timestamp = %d', array(time()));
+}
+
 // Drupal bootstrap.
 require_once './includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_DATABASE);
@@ -10,7 +38,7 @@ function ais_shutdown() {
 }
 
 // Load settings (variable_get dos not work yet).
-$url = db_fetch_object(db_query('SELECT value FROM variable WHERE name = \'%s\'', array('ais_url')))->value;
+$url = db_fetch_object(db_query('SELECT value FROM {variable} WHERE name = \'%s\'', array('ais_url')))->value;
 if (!$url) {
   echo "URL was not defined in AIS cron script.";
   exit;
@@ -18,7 +46,7 @@ if (!$url) {
 $url = unserialize($url);
 
 // Load settings (variable_get dos not work yet).
-$call = db_fetch_object(db_query('SELECT value FROM variable WHERE name = \'%s\'', array('ais_call')))->value;
+$call = db_fetch_object(db_query('SELECT value FROM {variable} WHERE name = \'%s\'', array('ais_call')))->value;
 if (!$call) {
   echo "Call sign was not defined in AIS cron script.";
   exit;
@@ -32,16 +60,37 @@ $xml = simplexml_load_file($url);
 foreach ($xml->marker as $mark) {
   $attributes = $mark->attributes();
   if ($attributes->call == $call) {
-    // Store data in db
+    // Build values that should be stored.
     $values = array(
       $attributes->lat, $attributes->lon,
       $attributes->name, $attributes->mmsi,
       $attributes->imo, $attributes->call,
       $attributes->speed, $attributes->course,
-      $attributes->type,
+      $attributes->type, time(),
     );
-    db_query("INSERT INTO {ais} VALUES ('', %f, %f, '%s', %d, %d, '%s', %f, %f, %d)", $values);
+
+    // Check if the position have moved since laste time
+    $new_pos = FALSE;
+    $result = db_fetch_object(db_query('SELECT * FROM {ais} ORDER BY id DESC LIMIT 1'));
+    if ($result) {
+      $distance = round(ais_distance((float)$result->long, (float)$result->lat,
+                                     (float)$attributes->lon, (float)$attributes->lat), 5);
+      // Check if lat have changed.
+      if ((float)$distance > 0.25) {
+        // Insert new record.
+        ais_insert($values);
+      }
+      else {
+        // Update record.
+        ais_update($result->id, $values);
+      }
+    }
+    else {
+      // Insert new record.
+      ais_insert($values);
+    }
+    
+    // End loop
+    break;
   }
 }
-
-echo "cron";
